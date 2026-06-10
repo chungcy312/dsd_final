a123d2f5c5e4e326e4533c81b76905b0d686b77a
Danielyyy123 <leewen183@gmail.com>
2026-06-10 14:24:10 +0800

    DSD_Final_G1 (code only)

---
+++ DSD_Final_G1/01_RTL/01_rtl.f
@@ -0,0 +1,119 @@
+########################################################
+# Active Defines                                       #
+########################################################
+// +define+DEBUG_PC
+// +define+DEBUG_IF_OUT
+//pattern need to check, no mod
+// +define+noHazard
+//+define+hasHazard
+// +define+BrPred
+// +define+Scaling
+// +define+compression
+// +define+compression_uncompressed
+// +define+QSort_uncompressed
+// +define+QSort
+// +define+Conv
+// +define+Conv_uncompressed
+// +define+Mul
+
+// +define+LFSR_HIST
+// +define+LFSR_HIST_short
+########################################################
+# TESTBED                                              #
+########################################################
+../00_TESTBED/testbench/Final_tb.v
+
+########################################################
+# Memory                                               #
+########################################################
+../00_MEMORY/slow_memory_random_latency.v
+../00_MEMORY/slow_memory.v
+../00_MEMORY/fast_memory.v
+
+########################################################
+# Flush Instruction Definition                         #
+########################################################
+../00_TESTBED/testbench/flush_inst_define.v
+
+########################################################
+# DUT                                                  #
+########################################################
+CHIP.v
+
+
+########################################################
+# Standard cell library                                #
+########################################################
+// -y /usr/cad/synopsys/synthesis/cur/dw/sim_ver +libext+.v
+// +incdir+/usr/cad/synopsys/synthesis/cur/dw/sim_ver/+
+
+########################################################
+# Dump FSDB                                            #
+########################################################
+// +define+FSDB
+
+########################################################
+# Defines for debug                                    #
+########################################################
+
+
+########################################################
+# Pattern Definition                                   #
+########################################################
+// +define+noHazard
+//+define+hasHazard
+// +define+customHazard
+// +define+hazEXMEMForward
+// +define+hazMEMWBForward
+// +define+hazStoreDataForward
+// +define+hazLoadUse
+// +define+hazLoadBranch
+// +define+hazBranchForward
+// +define+hazJALFlush
+// +define+hazJALRForward
+// +define+hazJALRNegImm
+// +define+hazCallReturn
+// +define+hazDCacheConflictFlush
+// +define+hazFibOnly
+// +define+hazBubbleOnly
+// +define+hazFibNoOutput
+// +define+hazAddiBneLoop
+// +define+hazAdjacentAddiBne
+// +define+hazBneBack28
+// +define+hazFibBodyLoop
+// +define+hazFibBodyLong
+// +define+hazFibBodyGap
+// +define+hazFibInitBodyNoGap
+// +define+hazBranchFlushJalr
+// +define+hazStoreBranchFlushJalr
+// +define+dbgRedirectUnalign32
+// +define+QSort
+// +define+dbgCLoadStoreForward
+// +define+dbgSltCBranchForward
+// +define+dbgCSwapMini
+// +define+dbgCBackwardLoop
+
+// +define+BrPred
+// +define+Scaling
+// +define+compression
+// +define+compression_uncompressed
+// +define+DEBUG_PC
+// +define+dbgCJalReturn
+// +define+dbgCBranch
+// +define+dbgCrossLoadUse
+// +define+dbgQSortCallShape
+// +define+dbgNestedCall
+// +define+dbgCSlliLoadAddr
+// +define+dbgCLoadStoreForward
+// +define+dbgSltCBranchForward
+// +define+dbgCSwapMini
+// +define+dbgCBackwardLoop
+// +define+dbgRedirectUnalign32
+// +define+QSort_uncompressed
+// +define+QSort
+// +define+Conv
+// +define+Conv_uncompressed
+// +define+Mul
+
+// +define+LFSR_HIST
+// +define+LFSR_HIST_short
